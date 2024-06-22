02546d04e90e8245df27ea74c00ef96145c99d4b
Hari <haricharansuraram@gmail.com>
2024-06-23 02:01:11 +0530

    t7:deleted some files

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,11 +1,9 @@
 import React from 'react';
-
 import Hero from '../components/Hero';
 import Statistics from '../components/Statistics';
 function Home() {
   return (
     <div>
-      
       <Hero/>
       <Statistics/>
     </div>
